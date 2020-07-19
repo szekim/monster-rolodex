1f8a644bee084ae36f7e3441dfc2dab3657b1641
Sze Kim Leong <szekim.leong@siemens.com>
2020-07-19 16:36:04 +0800

    Updates

---
+++ precache-manifest.74afee71d1e5177c1691da6c0640cd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4f53a706bdf3db21bee266bdee071a",
+    "url": "/szekim/monster-rolodex.git/index.html"
+  },
+  {
+    "revision": "ac4881a2595ad5ffa708",
+    "url": "/szekim/monster-rolodex.git/static/css/main.6298b421.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/szekim/monster-rolodex.git/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/szekim/monster-rolodex.git/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4881a2595ad5ffa708",
+    "url": "/szekim/monster-rolodex.git/static/js/main.dc38f44b.chunk.js"
+  },
+  {
+    "revision": "b181a76462aef1d219e1",
+    "url": "/szekim/monster-rolodex.git/static/js/runtime-main.551595a1.js"
+  }
+]);
